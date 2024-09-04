421e18e1e7f4ee0bf44c48984e887960114438b9
Philip osir Otieno <philiposir2@gmail.com>
2024-09-05 00:29:41 +0300

    updates

--- lib/student.py
+++ lib/student.py
@@ -1,5 +1,23 @@
 class Student:
-    pass
+    # def __init__(self, first_name, last_name):
+    #     self.first_name = first_name
+    #     self.last_name = last_name
+        
+    def hello(self):
+        print("Hey there! I'm so excited to learn stuff.")
+        
+    def raise_hand(self):
+        print("Pick me!")
 
 class ChattyStudent(Student):
-    pass
+    # def __init__(self, first_name, last_name):
+    #     super().__init__(first_name, last_name)
+        
+    def hello(self):
+        super().hello()
+        print("How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...")
+
+    def raise_hand(self):
+        for _ in range(10):
+            super().raise_hand()
+        
